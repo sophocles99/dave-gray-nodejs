c3ef47b2e571f9cd7cb26821865bb81c86ba5006
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-12-02 18:54:10 +0000

    create refreshToken controller and route

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const express = require("express");
+const path = require("path");
+
 const corsOptions = require("./config/corsOptions");
-const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
 const verifyJWT = require("./middleware/verifyJWT");
+const { logger } = require("./middleware/logEvents");
+
+const app = express();
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
@@ -20,6 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 // built-in middleware for json
 app.use(express.json());
 
+// middleware for cookies
+app.use(cookieParser());
+
 //serve static files
 app.use("/", express.static(path.join(__dirname, "/public")));
 
@@ -27,6 +33,7 @@ app.use("/", express.static(path.join(__dirname, "/public")));
 app.use("/", require("./routes/root"));
 app.use("/register", require("./routes/register"));
 app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
 
 app.use(verifyJWT);
 app.use("/employees", require("./routes/api/employees"));
