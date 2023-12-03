98c2f21e47ad9b291784c2833f3ea56ba6a893ab
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-12-03 08:49:55 +0000

    install mongoose, create connection, wait for open connection before starting server

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 
+const connectDB = require("./config/dbConnection");
 const corsOptions = require("./config/corsOptions");
 const credentials = require("./middleware/credentials");
 const errorHandler = require("./middleware/errorHandler");
@@ -12,6 +14,9 @@ const { logger } = require("./middleware/logEvents");
 
 const app = express();
 const PORT = process.env.PORT || 3500;
+
+// Connect to MongoDB
+connectDB();
 
 // custom middleware logger
 app.use(logger);
@@ -56,4 +61,7 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
