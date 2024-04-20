9b5bc137604e241984ca58985ceebedb07603352
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2024-04-20 12:50:02 +0100

    send 400 Bad Request for register request without username or password

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -5,7 +5,7 @@ const handleNewUser = async (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
     return res
-      .status(404)
+      .status(400)
       .json({ message: "Username and password are required" });
   }
   const duplicate = await User.findOne({ username });
