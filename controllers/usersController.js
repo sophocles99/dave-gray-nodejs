aa19a809731fcca9c161547a36df6c78b1081c18
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2024-04-20 13:20:17 +0100

    check id is valid MongoDB ObjectId before finding in DB

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,33 +1,44 @@
-const User = require('../model/User');
+const User = require("../model/User");
+const { isValidObjectId } = require("mongoose");
 
 const getAllUsers = async (req, res) => {
-    const users = await User.find();
-    console.log("getAllUsers controller")
-    if (!users) return res.status(204).json({ 'message': 'No users found' });
-    res.json(users);
-}
+  const users = await User.find();
+  console.log("getAllUsers controller");
+  if (!users) return res.status(204).json({ message: "No users found" });
+  res.json(users);
+};
 
 const deleteUser = async (req, res) => {
-    if (!req?.body?.id) return res.status(400).json({ "message": 'User ID required' });
-    const user = await User.findOne({ _id: req.body.id }).exec();
-    if (!user) {
-        return res.status(204).json({ 'message': `User ID ${req.body.id} not found` });
-    }
-    const result = await user.deleteOne({ _id: req.body.id });
-    res.json(result);
-}
+  if (!req?.body?.id)
+    return res.status(400).json({ message: "User ID required" });
+  if (!isValidObjectId(req.body.id))
+    return res.status(400).json({ message: "invalid id" });
+  const user = await User.findOne({ _id: req.body.id }).exec();
+  if (!user) {
+    return res
+      .status(204)
+      .json({ message: `User ID ${req.body.id} not found` });
+  }
+  const result = await user.deleteOne({ _id: req.body.id });
+  res.json(result);
+};
 
 const getUser = async (req, res) => {
-    if (!req?.params?.id) return res.status(400).json({ "message": 'User ID required' });
-    const user = await User.findOne({ _id: req.params.id }).exec();
-    if (!user) {
-        return res.status(204).json({ 'message': `User ID ${req.params.id} not found` });
-    }
-    res.json(user);
-}
+  if (!req?.params?.id)
+    return res.status(400).json({ message: "User ID required" });
+  if (!isValidObjectId(req.params.id))
+    return res.status(400).json({ message: "invalid id" });
+  const user = await User.findOne({ _id: req.params.id }).exec();
+  if (!user) {
+    return res
+      .status(204)
+      .json({ message: `User ID ${req.params.id} not found` });
+  }
+  res.json(user);
+};
 
 module.exports = {
-    getAllUsers,
-    deleteUser,
-    getUser
-}
+  getAllUsers,
+  deleteUser,
+  getUser,
+};
